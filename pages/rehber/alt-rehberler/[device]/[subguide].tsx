74d02ef51f027eb5385ce668c33321606473e54d
Berat <beratprodur2005@email.com>
2025-09-19 13:52:59 +0300

    Boş sayfayı düzenledim

---
+++ pages/rehber/alt-rehberler/[device]/[subguide].tsx
@@ -0,0 +1,13 @@
+// pages/rehber/alt-rehberler/[device]/[subguide].tsx
+
+import React from 'react';
+
+const SubGuidePage = () => {
+  return (
+    <div>
+      <h1>Bu bir alt rehber sayfasıdır.</h1>
+    </div>
+  );
+};
+
+export default SubGuidePage;
